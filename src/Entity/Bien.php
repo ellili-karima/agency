bcfb57037877738822aeb30324b3c82065a2391a
ellilikarima <mlayah1988@gmail.com>
2022-04-25 09:09:54 +0200

    fix

---
+++ src/Entity/Bien.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BienRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BienRepository::class)]
+class Bien
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $titre;
+
+    #[ORM\Column(type: 'integer')]
+    private $nbrepieces;
+
+    #[ORM\Column(type: 'float')]
+    private $surface;
+
+    #[ORM\Column(type: 'float')]
+    private $prix;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $localisation;
+
+    #[ORM\Column(type: 'string', length: 15)]
+    private $type;
+
+    #[ORM\Column(type: 'integer')]
+    private $etage;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private $transactiontype;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $description;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateconstruction;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getNbrepieces(): ?int
+    {
+        return $this->nbrepieces;
+    }
+
+    public function setNbrepieces(int $nbrepieces): self
+    {
+        $this->nbrepieces = $nbrepieces;
+
+        return $this;
+    }
+
+    public function getSurface(): ?float
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(float $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getLocalisation(): ?string
+    {
+        return $this->localisation;
+    }
+
+    public function setLocalisation(string $localisation): self
+    {
+        $this->localisation = $localisation;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getEtage(): ?int
+    {
+        return $this->etage;
+    }
+
+    public function setEtage(int $etage): self
+    {
+        $this->etage = $etage;
+
+        return $this;
+    }
+
+    public function getTransactiontype(): ?string
+    {
+        return $this->transactiontype;
+    }
+
+    public function setTransactiontype(string $transactiontype): self
+    {
+        $this->transactiontype = $transactiontype;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateconstruction(): ?\DateTimeInterface
+    {
+        return $this->dateconstruction;
+    }
+
+    public function setDateconstruction(\DateTimeInterface $dateconstruction): self
+    {
+        $this->dateconstruction = $dateconstruction;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Photo.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhotoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhotoRepository::class)]
+class Photo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 40)]
+    private $photo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
